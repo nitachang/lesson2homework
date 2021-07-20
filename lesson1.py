db52d0bd173a19227e88257d632f4ed9457913c8
nitachang <80136827+nitachang@users.noreply.github.com>
2021-07-20 18:22:45 +0800

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 19 11:25:58 2021
+
+@author: nitac
+"""
+
+print(1)
+print(3.4)
+print('hello world')
+
+print(type('**'))
+
+
+x = 2
+y = '小明'
+print('x+y')
+x=str(x)
+print('x+y')
+print('xy')
+
+
+print('''  "'  ''')
+
+
+name = input('名字?')
+print(name)
+
+     
+r = input('半徑?')
+r = float(r)
+ans = 2 * r * 3.14
+print(ans)
+
+
+
